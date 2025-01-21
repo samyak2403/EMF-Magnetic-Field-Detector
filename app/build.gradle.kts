5b9acd669a3dce55e8b37ba04c2dcea1cb3397cf
Samyak Kamble <samyakkamble6659@gmail.com>
2025-01-21 21:59:35 +0530

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.samyak2403.emf_magneticfielddetector"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.samyak2403.emf_magneticfielddetector"
